b8d563b1282ade03f16455aa7b0828c5d2caa412
Sydney <sydneysirois17@gmail.com>
2022-05-10 21:11:33 -0400

    blank functions added

---
+++ controllers/movie.js
@@ -0,0 +1,19 @@
+const movies = require('../movies')
+
+const getAllMovies = () => {
+
+}
+
+const getMovieByTitle = () => {
+
+}
+
+const getMovieByDirector = () => {
+    
+}
+
+module.exports = {
+    getAllMovies, 
+    getMovieByTitle, 
+    getMovieByDirector
+}
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const { getAllMovies, getMovieByTitle, getMovieByDirector } = require('./controllers/movie')
 
 const app = express()
+
+app.get('/movies', getAllMovies)
+
+app.get('/movies/:title', getMovieByTitle)
+
+app.get('/movies/:director', getMovieByDirector)
 
 app.listen(1300, () => {
   // eslint-disable-next-line no-console
