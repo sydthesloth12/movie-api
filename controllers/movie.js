5bffc7a9594fa0de80faf2c530a921d6a23b01ae
Sydney <sydneysirois17@gmail.com>
2022-05-16 11:32:49 -0400

    current progress

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,19 +1,29 @@
 const movies = require('../movies')
 
-const getAllMovies = () => {
-
+const getAllMovies = (request, response) => {
+  return response.status(200).send(movies)
 }
 
-const getMovieByTitle = () => {
+const getMovieByTitle = (request, response) => {
+  const movie = request.params.title
+  const foundMovie = movies.includes((movie) => {
+    return movie.title === movie
+  })
 
+  if (foundMovie) {
+    return response.status(200).send(foundMovie)
+  } else {
+    return response.status(404).send('Movie not found')
+  }
 }
+
 
 const getMovieByDirector = () => {
-    
+
 }
 
 module.exports = {
-    getAllMovies, 
-    getMovieByTitle, 
-    getMovieByDirector
+  getAllMovies,
+  getMovieByTitle,
+  getMovieByDirector
 }
